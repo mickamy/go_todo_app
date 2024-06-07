1cf074c9b53384edc84d1b5a6f53ee86b288d83e
mickamy <tetsuro@mickamy.com>
2024-06-07 21:22:03 +0900

    learn: SEC-054

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+			}
+		}),
+	)
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+}
