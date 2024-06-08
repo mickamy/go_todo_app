6b5f8090e1eee6c2c6769e69e632d2844b908ad5
mickamy <tetsuro@mickamy.com>
2024-06-08 19:00:25 +0900

    feat: create user registerer

--- service/interface.go
+++ service/interface.go
@@ -7,11 +7,15 @@ import (
 	"github.com/mickamy/go_todo_app/store"
 )
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskLister
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskLister UserRegisterer
 type TaskAdder interface {
 	AddTask(ctx context.Context, db store.Executor, t *entity.Task) error
 }
 
 type TaskLister interface {
 	ListTasks(ctx context.Context, db store.Queryer) (entity.Tasks, error)
+}
+
+type UserRegisterer interface {
+	RegisterUser(ctx context.Context, db store.Executor, u *entity.User) error
 }
